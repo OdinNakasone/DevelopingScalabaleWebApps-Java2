3f0e68103fc59b71fd67e867a847d9060fb3d461
Odin Nakasone <57544047+OdinNakasone@users.noreply.github.com>
2021-12-21 14:31:00 -1000

    Delete college-jpa directory

--- college-jpa/src/main/java/nakasone/odin/collegejpa/model/InstructorJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package nakasone.odin.collegejpa.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorJpaRepository extends JpaRepository<Instructor, Integer> {
-
-}
